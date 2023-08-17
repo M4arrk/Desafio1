928c9d37b794e7587e7defcd9c360e9d2f11df96
Marcos Gonzalez <142464847+M4arrk@users.noreply.github.com>
2023-08-17 00:33:25 -0300

    Add files via upload

---
+++ NumeroFlotanteGonzalez.java
@@ -0,0 +1,16 @@
+package Desafio1;
+
+//Esta es la primera clase y el Desafio 1
+public class NumeroFlotanteGonzalez {
+    // Creo el metodo
+    public static void main(String[] args) {
+        // Variable del Numero entero
+        int numeroEnetero = 30;
+        // Variable del numero Float
+        float numeroFloat = numeroEnetero;
+        // Se imprimen por consola los resultados
+        System.out.println("El numero Entero es: " + numeroEnetero);
+        System.out.println("El numero Float es: " + numeroFloat);
+
+    }
+}
